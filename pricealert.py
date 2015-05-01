27d538d49b2369d01132f289570d3195bdcc61db
Cedric Charly <cedric.charly@gmail.com>
2015-04-30 20:32:08 -0500

    Added support for using multiple subreddits

--- pricealert.py
+++ pricealert.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import sys
+import json
 import praw
 import twilio
+
 from twilio.rest import TwilioRestClient
-import json
+from requests.exceptions import HTTPError
 
 SETTINGS = "settings.json"
 INFO = "queue.json"
@@ -30,14 +32,26 @@ def main():
 
     new_data = False
     reddit_instance = praw.Reddit("PriceAlert price notifer - /u/Charly")
-    fmf_new_queue = list(reddit_instance.get_subreddit("frugalmalefashion").get_new(limit=10))
-    to_notify = []
     feed_map = get_saved_submissions(INFO)
 
-    for item in fmf_new_queue:
+    new_queue = []
+    subreddit_list = settings_map["subreddits"]
+    for tester in subreddit_list:
+        try:
+            reddit_instance.get_subreddit(tester).fullname
+        except HTTPError:
+            print("Could not find subreddit {}, aborting script".format(tester))
+            sys.exit(1)
+
+    for subreddit in subreddit_list:
+        new_queue.extend(list(reddit_instance.get_subreddit(subreddit).get_new(limit=10)))
+
+    to_notify = []
+    for item in new_queue:
         if item.fullname not in feed_map:
             new_data = True
-            feed_map[item.fullname] = [item.title, item.url, item.permalink, item.created_utc, item.short_link]
+            feed_map[item.fullname] = [item.title, item.url, item.permalink, item.created_utc,
+                                       item.subreddit.url, item.short_link]
             to_notify.append(item.fullname)
 
     if new_data:
@@ -46,13 +60,14 @@ def main():
             auth_token = settings_map["auth_token"]
             twilio_client = TwilioRestClient(account_sid, auth_token)
             for post in to_notify:
+                metadata = feed_map[post]
                 message_body = ""
                 if metadata[1] == metadata[2]:
                     message_body = "\nReddit (self): {}\n".format(metadata[-1])
                 else:
                     message_body = "\nURL: {}\nReddit: {}\n".format(metadata[1], metadata[-1])
-                metadata = feed_map[post]
-                message_body = message_body + "\nFMF: {}\n".format(metadata[0].encode('ascii', 'ignore'))
+                message_body = message_body + "\n{}: {}\n".format(metadata[4],
+                                                                  metadata[0].encode('ascii', 'ignore'))
 
                 message = twilio_client.messages.create(body=message_body,
                                                         to=settings_map["personal_number"],
